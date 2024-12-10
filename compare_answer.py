776f98b5bd89c14d295b8e767c1794980732a32e
HuXin0817 <202219120810@stu.cdut.edu.cn>
2024-12-10 19:37:47 +0800

    get_replace_cost

--- compare_answer.py
+++ compare_answer.py
@@ -4,20 +4,22 @@ VOWELS = set("aeiou")
 CONSONANTS = set("bcdfghjklmnpqrstvwxyz")
 
 
+def get_replace_cost(u_char: str, a_char: str):
+    if u_char in VOWELS and a_char in VOWELS:
+        return 0.5
+    elif u_char in CONSONANTS and a_char in CONSONANTS:
+        return 0.8
+    else:
+        return 1.0
+
+
 def weighted_ratio(user_answer, answer):
     ops = editops(user_answer, answer)
     total_cost = 0
 
     for op, i, j in ops:
         if op == "replace":
-            u_char = user_answer[i]
-            a_char = answer[j]
-            if u_char in VOWELS and a_char in VOWELS:
-                total_cost += 0.5
-            elif u_char in CONSONANTS and a_char in CONSONANTS:
-                total_cost += 0.8
-            else:
-                total_cost += 1
+            total_cost += get_replace_cost(user_answer[i], answer[j])
         else:
             total_cost += 1
 
